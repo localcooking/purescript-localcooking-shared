d8b939ace2964f2010d2fc6a42ed3f0d2f64d901
Athan Clark <athan.clark@gmail.com>
2018-05-27 13:15:17 -0600

    can't parse datetime yet

--- src/LocalCooking/Semantics/Common.purs
+++ src/LocalCooking/Semantics/Common.purs
@@ -1,11 +1,15 @@
 module LocalCooking.Semantics.Common where
 
+import LocalCooking.Database.Schema (StoredUserId)
+import LocalCooking.Common.User.Role (UserRole)
 import Facebook.Types (FacebookUserId)
 
 import Prelude
+import Data.DateTime (DateTime)
 import Data.Maybe (Maybe)
 import Data.Generic (class Generic, gEq, gShow)
 import Data.Argonaut (class EncodeJson, class DecodeJson, encodeJson, decodeJson, fail, (:=), (~>), jsonEmptyObject, (.?))
+import Text.Email.Validate (EmailAddress)
 import Test.QuickCheck (class Arbitrary, arbitrary)
 import Test.QuickCheck.Gen (oneOf)
 
@@ -37,3 +41,52 @@ instance decodeJsonSocialLoginForm :: DecodeJson SocialLoginForm where
     o <- decodeJson json
     fb <- o .? "fb"
     pure (SocialLoginForm {fb})
+
+
+newtype User = User
+  { id             :: StoredUserId
+  , created        :: DateTime -- FIXME SHIT, isomorphic to UTCTime?
+  , email          :: EmailAddress
+  , social         :: SocialLoginForm
+  , emailConfirmed :: Boolean
+  , roles          :: Array UserRole
+  }
+
+derive instance genericUser :: Generic User
+
+instance arbitraryUser :: Arbitrary User where
+  arbitrary = do
+    id <- arbitrary
+    created <- arbitrary
+    email <- arbitrary
+    social <- arbitrary
+    emailConfirmed <- arbitrary
+    roles <- arbitrary
+    pure (User {id,created,email,social,emailConfirmed,roles})
+
+instance eqUser :: Eq User where
+  eq = gEq
+
+instance showUser :: Show User where
+  show = gShow
+
+instance encodeJsonUser :: EncodeJson User where
+  encodeJson (User {id,created,email,social,emailConfirmed,roles})
+    =  "id" := id
+    ~> "created" := created
+    ~> "email" := email
+    ~> "social" := social
+    ~> "emailConfirmed" := emailConfirmed
+    ~> "roles" := roles
+    ~> jsonEmptyObject
+
+instance decodeJsonUser :: DecodeJson User where
+  decodeJson json = do
+    o <- decodeJson json
+    id <- o .? "id"
+    created <- o .? "created"
+    email <- o .? "email"
+    social <- o .? "social"
+    emailConfirmed <- o .? "emailConfirmed"
+    roles <- o .? "roles"
+    pure (User {id,created,email,social,emailConfirmed,roles})

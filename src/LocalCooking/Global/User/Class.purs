5929006036b54f8709db57b6d1b0710b9ac4d1ff
Athan Clark <athan.clark@gmail.com>
2018-06-07 21:16:52 -0600

    demanding user field in userDetails

--- src/LocalCooking/Global/User/Class.purs
+++ src/LocalCooking/Global/User/Class.purs
@@ -1,11 +1,11 @@
 module LocalCooking.Global.User.Class where
 
-import Text.Email.Validate (EmailAddress)
-import LocalCooking.Common.User.Role (UserRole)
+-- import Text.Email.Validate (EmailAddress)
+-- import LocalCooking.Common.User.Role (UserRole)
+import LocalCooking.Semantics.Common (User)
 
 
 -- | Fields assumed to be supported by the subsidiary site's user details -
 -- | additional fields might include customer details, chef details, etc.
 class UserDetails userDetails where
-  getEmailAddress :: userDetails -> EmailAddress
-  getUserRoles :: userDetails -> Array UserRole
+  getUser :: userDetails -> User

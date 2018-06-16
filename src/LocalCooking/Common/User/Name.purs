3087471aa30d89ba01cf50cc847d2aeff3184e83
Athan Clark <athan.clark@gmail.com>
2018-06-16 17:08:16 -0600

    using nonempty name

--- src/LocalCooking/Common/User/Name.purs
+++ src/LocalCooking/Common/User/Name.purs
@@ -1,32 +1,47 @@
 module LocalCooking.Common.User.Name where
 
 import Prelude
+import Data.Maybe (Maybe (..))
 import Data.Generic (class Generic)
-import Data.Argonaut (class EncodeJson, class DecodeJson)
+import Data.Argonaut (class EncodeJson, class DecodeJson, encodeJson, decodeJson, fail)
 import Data.NonEmpty (NonEmpty (..))
+import Data.Array (uncons) as Array
 import Data.String.Yarn (words, unwords) as String
 import Data.String.NonEmptyAlpha (NonEmptyAlphaString (..))
 import Test.QuickCheck (class Arbitrary, arbitrary)
 import Test.QuickCheck.Gen (arrayOf1)
 
 
-newtype Name = Name (Array String)
+newtype Name = Name (NonEmpty Array String)
 
 derive instance genericName :: Generic Name
 derive newtype instance eqName :: Eq Name
 derive newtype instance ordName :: Ord Name
-derive newtype instance encodeJsonName :: EncodeJson Name
-derive newtype instance decodeJsonName :: DecodeJson Name
+-- derive newtype instance encodeJsonName :: EncodeJson Name
+-- derive newtype instance decodeJsonName :: DecodeJson Name
 
 instance arbitraryName :: Arbitrary Name where
   arbitrary = do
-    NonEmpty n ns <- arrayOf1 $ do
+    ns <- arrayOf1 $ do
       NonEmptyAlphaString x <- arbitrary
       pure x
-    pure $ Name $ [n] <> ns
+    pure (Name ns)
 
 instance showName :: Show Name where
-  show (Name ns) = String.unwords ns
+  show (Name (NonEmpty n ns)) = String.unwords ([n] <> ns)
 
-name :: String -> Name
-name = Name <<< String.words
+instance encodeJsonName :: EncodeJson Name where
+  encodeJson (Name (NonEmpty n ns)) = encodeJson ([n] <> ns)
+
+instance decodeJsonName :: DecodeJson Name where
+  decodeJson json = do
+    as <- decodeJson json
+    case Array.uncons as of
+      Nothing -> fail "Empty Name"
+      Just {head,tail} -> pure $ Name $ NonEmpty head tail
+
+name :: String -> Maybe Name
+name x =
+  case Array.uncons (String.words x) of
+    Nothing -> Nothing
+    Just {head,tail} -> Just $ Name $ NonEmpty head tail

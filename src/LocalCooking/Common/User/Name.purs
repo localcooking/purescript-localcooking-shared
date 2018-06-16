14d3aa2adc26e6e6255907c2efe1375a82eefbca
Athan Clark <athan.clark@gmail.com>
2018-06-16 17:02:14 -0600

    nonEmptyAlpha def, plus better name generator

--- src/LocalCooking/Common/User/Name.purs
+++ src/LocalCooking/Common/User/Name.purs
@@ -3,20 +3,30 @@ module LocalCooking.Common.User.Name where
 import Prelude
 import Data.Generic (class Generic)
 import Data.Argonaut (class EncodeJson, class DecodeJson)
-import Data.String.Yarn (words) as String
-import Test.QuickCheck (class Arbitrary)
+import Data.NonEmpty (NonEmpty (..))
+import Data.String.Yarn (words, unwords) as String
+import Data.String.NonEmptyAlpha (NonEmptyAlphaString (..))
+import Test.QuickCheck (class Arbitrary, arbitrary)
+import Test.QuickCheck.Gen (arrayOf1)
 
 
 newtype Name = Name (Array String)
 
 derive instance genericName :: Generic Name
-derive newtype instance arbitraryName :: Arbitrary Name
 derive newtype instance eqName :: Eq Name
 derive newtype instance ordName :: Ord Name
-derive newtype instance showName :: Show Name
 derive newtype instance encodeJsonName :: EncodeJson Name
 derive newtype instance decodeJsonName :: DecodeJson Name
 
+instance arbitraryName :: Arbitrary Name where
+  arbitrary = do
+    NonEmpty n ns <- arrayOf1 $ do
+      NonEmptyAlphaString x <- arbitrary
+      pure x
+    pure $ Name $ [n] <> ns
+
+instance showName :: Show Name where
+  show (Name ns) = String.unwords ns
 
 name :: String -> Name
 name = Name <<< String.words

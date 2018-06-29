aa78cf48597453f401190ce1129babf2af025d34
Athan Clark <athan.clark@gmail.com>
2018-06-28 19:29:36 -0600

    better show instance for permalinks

--- src/Data/String/Permalink.purs
+++ src/Data/String/Permalink.purs
@@ -17,11 +17,12 @@ derive instance genericPermalink :: Generic Permalink
 derive newtype instance arbitraryPermalink :: Arbitrary Permalink
 derive newtype instance eqPermalink :: Eq Permalink
 derive newtype instance ordPermalink :: Ord Permalink
-derive newtype instance showPermalink :: Show Permalink
 derive newtype instance encodeJsonPermalink :: EncodeJson Permalink
 derive newtype instance decodeJsonPermalink :: DecodeJson Permalink
 derive newtype instance isStringPermalink :: IsString Permalink
 
+instance showPermalink :: Show Permalink where
+  show (Permalink x) = x
 
 permalinkParser :: Parser Permalink
 permalinkParser = (Permalink <<< fromChars) <$> many1 (alphaNum <|> char '-' <|> char '_')

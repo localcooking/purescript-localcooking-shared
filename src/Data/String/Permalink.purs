dfb35bc03bc5de339d8cb1b149b14b1b18d37a81
Athan Clark <athan.clark@gmail.com>
2018-07-02 21:24:17 -0600

    proper arbitrary instance (disregarding digits, dashes and underscores)

--- src/Data/String/Permalink.purs
+++ src/Data/String/Permalink.purs
@@ -4,17 +4,17 @@ import Prelude
 import Data.Generic (class Generic)
 import Data.Argonaut (class EncodeJson, class DecodeJson)
 import Data.String.Yarn (class IsString, fromChars)
+import Data.String.NonEmptyAlpha (NonEmptyAlphaString (..))
 import Control.Alternative ((<|>))
 import Text.Parsing.StringParser (Parser)
 import Text.Parsing.StringParser.String (alphaNum, char)
 import Text.Parsing.StringParser.Combinators (many1)
-import Test.QuickCheck (class Arbitrary)
+import Test.QuickCheck (class Arbitrary, arbitrary)
 
 
 newtype Permalink = Permalink String
 
 derive instance genericPermalink :: Generic Permalink
-derive newtype instance arbitraryPermalink :: Arbitrary Permalink
 derive newtype instance eqPermalink :: Eq Permalink
 derive newtype instance ordPermalink :: Ord Permalink
 derive newtype instance encodeJsonPermalink :: EncodeJson Permalink
@@ -23,6 +23,9 @@ derive newtype instance isStringPermalink :: IsString Permalink
 
 instance showPermalink :: Show Permalink where
   show (Permalink x) = x
+
+instance arbitraryPermalink :: Arbitrary Permalink where
+  arbitrary = (\(NonEmptyAlphaString x) -> Permalink x) <$> arbitrary
 
 permalinkParser :: Parser Permalink
 permalinkParser = (Permalink <<< fromChars) <$> many1 (alphaNum <|> char '-' <|> char '_')

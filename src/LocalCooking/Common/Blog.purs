835ee93273342947e5266c39afe3ed4b53406f0b
Athan Clark <athan.clark@gmail.com>
2018-07-24 14:30:51 -0600

    enum instance for blog post variant

--- src/LocalCooking/Common/Blog.purs
+++ src/LocalCooking/Common/Blog.purs
@@ -2,9 +2,11 @@ module LocalCooking.Common.Blog where
 
 import Prelude
 import Data.Either (Either (..))
+import Data.Maybe (Maybe (..))
 import Data.NonEmpty (NonEmpty (..))
 import Data.Generic (class Generic, gEq, gCompare)
 import Data.Argonaut (class EncodeJson, class DecodeJson, decodeJson, encodeJson, fail)
+import Data.Enum (class Enum, class BoundedEnum, Cardinality (..))
 import Control.Alternative ((<|>))
 import Text.Parsing.StringParser (Parser, runParser)
 import Text.Parsing.StringParser.String (string)
@@ -31,6 +33,33 @@ instance eqBlogPostVariant :: Eq BlogPostVariant where
 
 instance ordBlogPostVariant :: Ord BlogPostVariant where
   compare = gCompare
+
+instance enumBlogPostVariant :: Enum BlogPostVariant where
+  succ x = case x of
+    CasualBlogPost -> Just BusinessBlogPost
+    BusinessBlogPost -> Just PersonalBlogPost
+    PersonalBlogPost -> Nothing
+  pred x = case x of
+    CasualBlogPost -> Nothing
+    BusinessBlogPost -> Just CasualBlogPost
+    PersonalBlogPost -> Just BusinessBlogPost
+
+instance boundedBlogPostVariant :: Bounded BlogPostVariant where
+  top = PersonalBlogPost
+  bottom = CasualBlogPost
+
+instance boundedEnumBlogPostVariant :: BoundedEnum BlogPostVariant where
+  cardinality = Cardinality 3
+  toEnum i
+    | i == 0 = Just CasualBlogPost
+    | i == 1 = Just BusinessBlogPost
+    | i == 2 = Just PersonalBlogPost
+    | otherwise = Nothing
+  fromEnum x = case x of
+    CasualBlogPost -> 0
+    BusinessBlogPost -> 1
+    PersonalBlogPost -> 2
+
 
 -- TODO Enum, Bounded, BoundedEnum
 
